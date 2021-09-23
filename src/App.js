d629fb0df0e4669212fe4451e924e59ed2a82ce1
Lucas-GSS <lucasgabrieldesouza9@gmail.com>
2021-09-23 16:08:40 -0300

    Exercicio 1 resolvido

--- src/App.js
+++ src/App.js
@@ -1,30 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>
-            src/App.js
-          </code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dogImage: undefined,
+    };
+    this.requestApi = this.requestApi.bind(this);
+  }
+
+  componentDidMount() {
+    this.requestApi();
+  }
+
+  async requestApi() {
+    const endPoint = 'https://dog.ceo/api/breeds/image/random';
+    const request = await fetch(endPoint);
+    const requestJson = await request.json();
+    this.setState({ dogImage: requestJson });
+  }
+
+  render() {
+    const { dogImage } = this.state;
+    const loading = <div>Loading...</div>;
+    return (
+      <div className="App">
+        { dogImage === undefined ? loading
+          : <img src={ dogImage.message } alt="Cachorro aleatório" /> }
+        <button type="button" onClick={ this.requestApi }>
+          Buscar outro Cachorro
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
